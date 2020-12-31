5b8b7e400f029b3374292a9d1813f39b44da01b1
Sundaei <tmdgur7389@naver.com>
2020-12-31 19:46:08 +0900

    asdfff

---
+++ 2.py
@@ -0,0 +1,22 @@
+import torch as tc
+x=tc.ones(2,2,requires_grad=True)
+print(x)
+y=x+2;print(y)
+print(y.grad_fn)
+z=y*y*3;print(z)
+out=z.mean();print(z,out)
+a=tc.randn(2,2);a=((a*3)/(a-1))
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b=(a*a).sum()
+print(b.grad_fn)
+out.backward()
+print(x.grad)
+x=tc.randn(3,requires_grad=True);y=x*2;print(y)
+while y.data.norm()<1000:
+    y=y*2
+print(y)
+v=tc.tensor([0.1,1.0,0.0001],dtype=tc.float)
+y.backward(v)
+print(x.grad)
