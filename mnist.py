1cfa7bd99ad225258911d9275e6747a9930e643b
Sundaei <tmdgur7389@naver.com>
2021-01-22 00:00:16 +0900

    mnist finish

--- mnist.py
+++ mnist.py
@@ -8,3 +8,59 @@ import random
 USE_CUDA=tc.cuda.is_available()
 device=tc.device("cuda"if USE_CUDA else "cpu")
 print("asdf:",device)
+random.seed(777)
+tc.manual_seed(777)
+if device=='cuda':
+    tc.cuda.manual_seed_all(777)
+training_epochs=15
+batch_size=100
+
+mnist_train=dsets.MNIST(root='MNIST_data/',train=True,
+transform=transforms.ToTensor(),download=True)
+
+mnist_test=dsets.MNIST(root='MNIST_data/',train=False,
+transform=transforms.ToTensor(),download=True)
+data_loader=DataLoader(dataset=mnist_train,
+batch_size=batch_size,shuffle=True,drop_last=True)
+
+linear=nn.Linear(784,10,bias=True).to(device)
+criterion=nn.CrossEntropyLoss().to(device)
+optimizer=tc.optim.SGD(linear.parameters(),lr=0.1)
+
+for epoch in range(training_epochs):
+    avg_cost=0
+    total_batch=len(data_loader)
+    for X,Y in data_loader:
+        X=X.view(-1,28*28).to(device)
+        Y=Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis=linear(X)
+        cost=criterion(hypothesis,Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost+=cost/total_batch
+    print('Epoch:','%04d'%(epoch+1),'cost=','{:.9f}'.format(avg_cost))
+print('Learning finished')
+
+with tc.no_grad():
+    X_test=mnist_test.test_data.view(-1,28*28).float().to(device)
+    Y_test=mnist_test.test_labels.to(device)
+
+    prediction=linear(X_test)
+    correct_prediction=tc.argmax(prediction,1)==Y_test
+    accuracy=correct_prediction.float().mean()
+    print('Accuracy:',accuracy.item())
+
+    r=random.randint(0,len(mnist_test)-1)
+    X_single_data=mnist_test.test_data[r:r+1].view(-1,28*28).float().to(device)
+    Y_single_data=mnist_test.test_labels[r:r+1].to(device)
+
+    print('Label:',Y_single_data.item())
+    single_prediction=linear(X_single_data)
+    print('Prediction:',tc.argmax(single_prediction,1).item())
+    plt.imshow(mnist_test.test_data[r:r+1].view(28,28),
+    cmap='Greys',interpolation='nearest')
+    plt.show()
+    
